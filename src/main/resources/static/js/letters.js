3702167ca368a19fecd0287c569bafa3df5ce329
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2019-04-22 02:11:35 +0200

    add HelloPage: first slide

---
+++ src/main/resources/static/js/letters.js
@@ -0,0 +1,20 @@
+var wrapper = document.querySelector(".letter");
+var text = document.querySelector(".spanForSliderFirst");
+
+var textCont = text.textContent;
+text.style.display = "none";
+
+for (var i = 0; i < textCont.length; i++) {
+  (function(i) {
+    setTimeout(function() {
+      // Created textNode to append
+      var texts = document.createTextNode(textCont[i])
+      var span = document.createElement('span');
+      span.appendChild(texts);
+
+      span.classList.add("wave");
+      wrapper.appendChild(span);
+
+    }, 75 * i);
+  }(i));
+}
