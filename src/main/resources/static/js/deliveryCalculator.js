f0fd3dc5cdab0107493ffa7e5afabbf7c6029f4d
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2019-05-03 01:08:41 +0200

    add loading and count distance to taxi

--- src/main/resources/static/js/deliveryCalculator.js
+++ src/main/resources/static/js/deliveryCalculator.js
@@ -8,16 +8,15 @@ function init() {
         MINIMUM_COST = 500,
         myMap = new ymaps.Map('map', {
             center: [53.9000000, 27.5666700],
-            center: [53.937722, 27.462818],
             zoom: 9,
             controls: []
+        }, {
+            searchControlProvider: 'yandex#search'
         }),
         // Создадим панель маршрутизации.
         routePanelControl = new ymaps.control.RoutePanel({
             options: {
-                position: {
-
-                }
+                position: {}
             }
         }),
         zoomControl = new ymaps.control.ZoomControl({
@@ -40,58 +39,82 @@ function init() {
         fromEnabled: false,
         from: 'Москва, Льва Толстого 16'
      });*/
+    var myPlacemark = new ymaps.Placemark([53.937722, 27.462818], {
+            balloonContentBody: [
 
+            ].join('')
+        },
+        {
+            preset: 'islands#redDotIcon'
+        });
+    myMap.geoObjects.add(myPlacemark);
+
+
+    routePanelControl.routePanel.state.set({
+        // Зададим тип маршрутизации - такси.
+        type: "taxi",
+        // Зададим адрес пункта назначения.
+        to: myPlacemark.geometry.getCoordinates(),
+        from: routePanelControl.routePanel.geolocate('from'),
+        // Отключим возможность задавать пункт отправления в поле ввода.
+        toEnabled: true
+    });
     myMap.controls.add(routePanelControl).add(zoomControl);
-
-    // Получим ссылку на маршрут.
+    // $('input[placeholder$="Откуда"]').val(routePanelControl.routePanel.geolocate('from'));
     routePanelControl.routePanel.getRouteAsync().then(function (route) {
-
-        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
         route.model.setParams({results: 1}, true);
-
-        // Повесим обработчик на событие построения маршрута.
         route.model.events.add('requestsuccess', function () {
-
             var activeRoute = route.getActiveRoute();
             if (activeRoute) {
-                // Получим протяженность маршрута.
                 var length = route.getActiveRoute().properties.get("distance"),
-                    // Вычислим стоимость доставки.
-                    price = calculate(Math.round(length.value / 1000)),
-                    // Создадим макет содержимого балуна маршрута.
-                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
-                        '<span>Расстояние: ' + length.text + '.</span><br/>' +
-                        '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + Math.round(price * 100) / 100 + ' р.</span>');
-                // Зададим этот макет для содержимого балуна.
+                    price = calculate(Math.round(length.value / 1000));
+                balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                    '<span>Расстояние: ' + length.text + '.</span><br/>' +
+                    '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + Math.round(price) + ' р.</span>');
+                myPlacemark.properties.set('balloonContentBody', '<button type="button" class="btn btn-warning">Aleksey</button><br></br>' + length.text);
                 route.options.set('routeBalloonContentLayout', balloonContentLayout);
-                // Откроем балун.
                 activeRoute.balloon.open();
+                if ($('input[placeholder$="Откуда"]').val() == '' && $('input[placeholder$="Куда"]').val() == '') {
+
+                }
+                else {
+                    $('.ymaps-2-1-73-route-panel__clear').click();
+                }
             }
         });
 
     });
 
-    // Функция, вычисляющая стоимость доставки.
+
     function calculate(routeLength) {
         return Math.max(routeLength * DELIVERY_TARIFF, MINIMUM_COST) * 0.0326;
     }
 
-    var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-        balloonContentBody: [
-            '<address>',
-            '<strong>Офис Яндекса в Москве</strong>',
-            '<br/>',
-            'Адрес: 119021, Москва, ул. Льва Толстого, 16',
-            '<br/>',
-            'Подробнее: <a href="https://company.yandex.ru/">https://company.yandex.ru</a>',
-            '</address>'
-        ].join('')
-    }, {
-        preset: 'islands#redDotIcon'
-    });
-    myMap.geoObjects.add(myPlacemark);
+
+    // routePanelControl.routePanel.state.set({
+    //     // Зададим тип маршрутизации - такси.
+    //     type: "taxi",
+    //     // Зададим адрес пункта назначения.
+    //     from:geolocation,
+    //     to: 'Павелецкий вокзал',
+    //     // Отключим возможность задавать пункт отправления в поле ввода.
+    //     toEnabled: false
+    // });
+
 }
 
 function test() {
     alert($('.ymaps-2-1-73-route-panel-input__input').val());
 }
+
+function change() {
+    // $("[id='newForMap']").each(function (){
+    //     $(this).attr("id", "map");
+    // });
+    $('#map').css({"opacity":'1'});
+    $('div.cssload-spin-box').toggleClass('newForAnimation');
+    $('div.cssload-spin-box').toggleClass('cssload-spin-box');
+    $('div.newForCars').toggleClass('newForCar');
+    $('div.newForCars').toggleClass('newForCars');
+}
+setTimeout(change, 10000);
