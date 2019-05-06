ee40ac5f68dd526ca2fc8546b97aba547ce5036d
Comrade312 <comrade3152@gmail.com>
2019-05-06 17:40:36 +0300

    Thymeleaf integration

--- src/main/java/taxiservice/controller/AdminMenuController.java
+++ src/main/java/taxiservice/controller/AdminMenuController.java
@@ -1,36 +1,31 @@
 package taxiservice.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import taxiservice.entity.User;
 import taxiservice.repository.UserRepo;
 
-
 import java.util.Map;
 
-
 @Controller
-//@PreAuthorize("hasAuthority('ADMIN')")
-public class MainController {
+@PreAuthorize("hasAuthority('ADMIN')")
+@RequestMapping("/admin")
+public class AdminMenuController {
 
     @Autowired
     private UserRepo userRepo;
 
-    @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
-        return "hello";
+    @GetMapping("")
+    public String greeting(Model model) {
+        return "adminMenu";
     }
 
-    @GetMapping("/main")
-    public String mainPage(@AuthenticationPrincipal User user, Model model) {
 
-        model.addAttribute("user", user);
-        return "MainPage.html";
-    }
 
 
 }
