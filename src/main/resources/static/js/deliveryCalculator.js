cb2b8b4dce74c96912b93df76e54f9e8aaab4050
Sergey Zdasuk <Sergey.zdasuk@tut.by>
2019-05-12 03:28:50 +0200

    destroy all markers,name taxi,car name, add design to user and taxi

--- src/main/resources/static/js/deliveryCalculator.js
+++ src/main/resources/static/js/deliveryCalculator.js
@@ -4,15 +4,11 @@ $(document).ready(function () {
         $('input[name$="username"]').val($('.btn.btn-outline-secondary span').text());
         $('input[name$="placeFrom"]').val($('input[placeholder$="Откуда"]').val());
         $('input[name$="placeTo"]').val($('input[placeholder$="Куда"]').val());
-        $('input[name$="price"]').val(map.get(1));
-
     });
 
 });
-function changeStyle(){
-    alert('s');
 
-}
+
 ymaps.ready(geoFindMe);
 
 function addFirstPriceToModal() {
@@ -33,10 +29,6 @@ function addThirdPriceToModal() {
 
 function change() {
     $('#map').css({"opacity": '1'});
-    $('.ymaps-2-1-73-route-panel__input ymaps-2-1-73-route-panel__input_from').css({"background-color": 'black'});
-    $('.ymaps-2-1-73-route-panel__input ymaps-2-1-73-route-panel__input_from').css({"position": 'absolute'});
-    $('.ymaps-2-1-73-route-panel__input ymaps-2-1-73-route-panel__input_from').css({"z-index": '10'});
-    $('.ymaps-2-1-73-route-panel__input ymaps-2-1-73-route-panel__input_from').css({"right": '70px'});
     $('div.cssload-spin-box').toggleClass('newForAnimation');
     $('div.cssload-spin-box').toggleClass('cssload-spin-box');
     changeTextOfInput();
@@ -51,12 +43,14 @@ function addInfoToInputFirst() {
 }
 
 function changeTextOfInput() {
-    $(".ymaps-2-1-73-route-panel-input__input")[0].setAttribute("onchange", "foo()");
-    $(".ymaps-2-1-73-route-panel-input__input")[1].setAttribute("onchange", "foo()");
+
+    $('input[placeholder$="Откуда"]').setAttribute("onchange", "foo()");
+    $('input[placeholder$="Куда"]').setAttribute("onchange", "foo()");
 }
 
 
 function foo() {
+
     if ($('input[placeholder$="Откуда"]').val() != '' && $('input[placeholder$="Куда"]').val() != '') {
         init($('input[placeholder$="Откуда"]').val(), '');
     }
@@ -70,24 +64,26 @@ let myMap;
 
 function init(latitude, longitude) {
     if (longitude == '') {
+
         countDistanceWithoutMyPlace();
         return;
     }
     // Стоимость за километр.
-    var DELIVERY_TARIFF = 20;
-    // Минимальная стоимость.
-    var MINIMUM_COST = 500;
-    myMap = new ymaps.Map('map', {
-        center: [53.9000000, 27.5666700],
-        zoom: 9,
-        controls: []
-    }, {
-        searchControlProvider: 'yandex#search'
-    }),
+
+    var DELIVERY_TARIFF = 20,
+        // Минимальная стоимость.
+        MINIMUM_COST = 500,
+        myMap = new ymaps.Map('map', {
+            center: [53.9, 27.55659],
+            zoom: 9,
+            controls: []
+        }),
         // Создадим панель маршрутизации.
         routePanelControl = new ymaps.control.RoutePanel({
             options: {
-                position: {}
+                // Добавим заголовок панели.
+                showHeader: true
+
             }
         }),
         zoomControl = new ymaps.control.ZoomControl({
@@ -104,98 +100,66 @@ function init(latitude, longitude) {
     routePanelControl.routePanel.options.set({
         types: {auto: true}
     });
-    myMap.controls.add(routePanelControl).add(zoomControl);
-    num = [
-        {
-            coordinates: [53.937722, 27.462818]
-        },
-        {
-            coordinates: [53.909938, 27.458761]
-        },
-        {
-            coordinates: [53.883729, 27.498733]
-        }
-    ];
+    // Если вы хотите задать неизменяемую точку "откуда", раскомментируйте код ниже.
+    /*routePanelControl.routePanel.state.set({
+        fromEnabled: false,
+        from: 'Москва, Льва Толстого 16'
+     });*/
 
+    myMap.controls.add(routePanelControl).add(zoomControl);
+
+    // Получим ссылку на маршрут.
+    routePanelControl.routePanel.getRouteAsync().then(function (route) {
+
+        // Зададим максимально допустимое число маршрутов, возвращаемых мультимаршрутизатором.
+        route.model.setParams({results: 1}, true);
+
+        // Повесим обработчик на событие построения маршрута.
+        route.model.events.add('requestsuccess', function () {
+
+            var activeRoute = route.getActiveRoute();
+            if (activeRoute) {
+                // Получим протяженность маршрута.
+                var length = route.getActiveRoute().properties.get("distance"),
+                    // Вычислим стоимость доставки.
+                    price = calculate(Math.round(length.value / 1000)),
+                    // Создадим макет содержимого балуна маршрута.
+                    balloonContentLayout = ymaps.templateLayoutFactory.createClass(
+                        '<span>Расстояние: ' + length.text + '.</span><br/>' +
+                        '<span style="font-weight: bold; font-style: italic">Стоимость доставки: ' + price + ' р.</span>');
+                // Зададим этот макет для содержимого балуна.
+                route.options.set('routeBalloonContentLayout', balloonContentLayout);
+                // Откроем балун.
+                activeRoute.balloon.open();
+                $('input[name$="price"]').val(price);
+            }
+        });
+
+    });
+    setTimeout(change(),1000);
+    // change();
+    changeTextOfInput();
+
+    function countDistanceWithoutMyPlace() {
+
+        $('#map').css({"opacity": '0'});
+        $('div.newForAnimation').toggleClass('cssload-spin-box');
+        $('div.newForAnimation').toggleClass('newForAnimation');
+        if ($('input[placeholder$="Откуда"]').val() == '' && $('input[placeholder$="Куда"]').val() == '') {
+        }
+        else {
+            setTimeout(change(),1000);
+            changeTextOfInput();
+        }
+
+
+    }
+
+    // Функция, вычисляющая стоимость доставки.
     function calculate(routeLength) {
         return routeLength * 0.5;
     }
 
-    function countDistanceWithoutMyPlace() {
-        $('#map').css({"opacity": '0'});
-        $('div.newForAnimation').toggleClass('cssload-spin-box');
-        $('div.newForAnimation').toggleClass('newForAnimation');
-        for (var i = 0; i < num.length; i++) {
-            if (i == 0) {
-
-
-                        var distanceMarkerTo = route.getHumanLength().replace(/&#160;км/i, '');
-                        var price;
-                        ymaps.route([$('input[placeholder$="Откуда"]').val(), $('input[placeholder$="Куда"]').val()], {mapStateAutoApply: true}).then(
-                            function (routeFirst) {
-                                myMap.geoObjects.add(routeFirst);
-                                price = calculate(routeFirst.getHumanLength().replace(/&#160;км/i, ''));
-                                map.set(1, price);
-                                myMap.geoObjects.remove(routeFirst);
-                            }
-
-
-                );
-            }
-
-
-            if (i == 1) {
-
-                ymaps.route([$('input[placeholder$="Откуда"]').val(), myPlacemarkSecond.geometry.getCoordinates()], {mapStateAutoApply: true}).then(
-                    function (route) {
-                        myMap.geoObjects.add(route);
-                        var distanceMarkerTo = route.getHumanLength().replace(/&#160;км/i, '');
-                        var price;
-                        ymaps.route([$('input[placeholder$="Откуда"]').val(), $('input[placeholder$="Куда"]').val()], {mapStateAutoApply: true}).then(
-                            function (routeSecond) {
-                                myMap.geoObjects.add(routeSecond);
-                                price = calculate(routeSecond.getHumanLength().replace(/&#160;км/i, ''));
-                                map.set(2, price);
-                                myPlacemarkSecond.properties.set('balloonContentBody', '<button type="button" class="btn btn-warning" onclick="addSecondPriceToModal()">Vitaliy</button><br></br>' + distanceMarkerTo + ' км<span id="priceSecond"> ' + price + ' р </span>');
-                                myMap.geoObjects.remove(routeSecond);
-                            }
-                        );
-                        myMap.geoObjects.remove(route);
-                    }
-                );
-            }
-
-
-            if (i == 2) {
-                ymaps.route([$('input[placeholder$="Откуда"]').val(), myPlacemarkThird.geometry.getCoordinates()], {mapStateAutoApply: true}).then(
-                    function (route) {
-                        myMap.geoObjects.add(route);
-                        var distanceMarkerTo = route.getHumanLength().replace(/&#160;км/i, '');
-                        var price;
-                        ymaps.route([$('input[placeholder$="Откуда"]').val(), $('input[placeholder$="Куда"]').val()], {mapStateAutoApply: true}).then(
-                            function (routeThird) {
-                                myMap.geoObjects.add(routeThird);
-                                price = calculate(routeThird.getHumanLength().replace(/&#160;км/i, ''));
-                                map.set(3, price);
-                                myPlacemarkThird.properties.set('balloonContentBody', '<button type="button" class="btn btn-warning" onclick="addThirdPriceToModal()">Andrey</button><br></br>' + distanceMarkerTo + ' км<span id="priceSecond"> ' + price + ' р </span>');
-                                myMap.geoObjects.remove(routeThird);
-                            }
-                        );
-                        myMap.geoObjects.remove(route);
-                    }
-                );
-                if ($('input[placeholder$="Откуда"]').val() == '' && $('input[placeholder$="Куда"]').val() == '') {
-
-                }
-                else {
-                    setTimeout(change, 5000);
-                    changeTextOfInput();
-                }
-            }
-
-
-        }
-    }
 }
 
 
@@ -208,21 +172,19 @@ function geoFindMe() {
     };
     navigator.geolocation.getCurrentPosition(success);
 }
-function get_coords(address)
-{
+
+function get_coords(address) {
     // Поиск координат
-    ymaps.geocode(address, { results: 1 }).then(function (res)
-        {
+    ymaps.geocode(address, {results: 1}).then(function (res) {
             // Выбираем первый результат геокодирования
             var firstGeoObject = res.geoObjects.get(0);
             var cords = firstGeoObject.geometry.getCoordinates();
 
-            alert(cords[0]+' '+cords[1])
+            alert(cords[0] + ' ' + cords[1])
 
 
         },
-        function (err)
-        {
+        function (err) {
             // Если геокодирование не удалось,
             // сообщаем об ошибке
             alert(err.message);
