e213a28a4b3a856e2ee71d9f93fed23b8725e377
Comrade312 <comrade3152@gmail.com>
2019-05-13 20:41:49 +0300

    Update registration

--- src/main/java/taxiservice/controller/RegistrationController.java
+++ src/main/java/taxiservice/controller/RegistrationController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import taxiservice.entity.Person;
 import taxiservice.entity.Role;
 import taxiservice.entity.User;
 import taxiservice.repository.UserRepo;
@@ -24,7 +26,11 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addUser(User user, Model model) {
+    public String addUser(User user,
+                          @RequestParam String name,
+                          @RequestParam String surname,
+                          @RequestParam String phoneNumber,
+                          Model model) {
         User userFromDb = userRepo.findByUsername(user.getUsername());
 
         if (userFromDb != null) {
@@ -34,6 +40,7 @@ public class RegistrationController {
 
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
+        user.setPerson(new Person(name, surname, phoneNumber));
         userRepo.save(user);
 
         return "redirect:/login";
